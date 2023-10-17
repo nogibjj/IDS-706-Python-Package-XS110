2ecba97a6a305f32e9f25833dace1daa732c670e
Xinyi Sheng <111578487+xs110@users.noreply.github.com>
2023-10-17 21:45:48 +0000

    finish

--- main.py
+++ main.py
@@ -1,63 +1,10 @@
-from databricks import sql
-import os
-from dotenv import load_dotenv
-from tabulate import tabulate
-
-# Load environment variables from .env
-load_dotenv()
-
-def run_query():
-    try:
-        # Replace with your environment variables or provide the actual values
-        server_hostname = os.getenv("DATABRICKS_HOST")
-        http_path = os.getenv("DATABRICKS_HTTP_PATH")
-        access_token = os.getenv("DATABRICKS_TOKEN")
-
-        # Establish a connection
-        with sql.connect(
-            server_hostname=server_hostname,
-            http_path=http_path,
-            access_token=access_token
-        ) as connection:
-            # Create a cursor
-            with connection.cursor() as cursor:
-                # Define the SQL query
-                sql_query = """
-                SELECT customerName, COUNT(O.orderNumber) as orderNum
-                FROM customers AS C
-                LEFT JOIN orders AS O ON C.customerNumber = O.customerNumber
-                GROUP BY customerName
-                HAVING customerName IS NOT NULL
-                ORDER BY orderNum DESC
-                LIMIT 10
-                """
-                # Execute the query
-                cursor.execute(sql_query)
-
-                # Fetch the result
-                result = cursor.fetchall()
+from mylib.query import run_query
 
 
-                # Print the result as a table
-                headers = ["Customer Name", "Order Number"]
-                table = tabulate(result, headers=headers, tablefmt="pretty")
-                print(table)
-                print("Query execution completed.")
-    except Exception as e:
-        print(f"Error: {e}")
-
-    finally:
-        try:
-            # Close cursor and connection
-            cursor.close()
-            connection.close()
-            print("Connection closed.")
-        except NameError:
-            # Handle the case where the cursor or connection was not defined
-            pass
-
-
+def main():
+    run_query()
+    return 1
 
 
 if __name__ == "__main__":
-    run_query()
+    main()
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="databricks-cli-tool",
+    version="0.1",
+    author="Xinyi Sheng",
+    author_email="xs110@duke.edu",
+    packages=find_packages(),
+    install_requires=[
+        "databricks",
+        "python-dotenv",
+        "tabulate",
+    ],
+    entry_points={
+        "console_scripts": [
+            "databricks-query = main:main",
+        ],
+    },
+)
