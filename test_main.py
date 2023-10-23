dee996c283d51878b9037fd48028cb63e5c8951a
Xinyi Sheng <111578487+xs110@users.noreply.github.com>
2023-10-23 02:34:00 +0000

    test

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ import main
 
 
 class Test_run_query(unittest.TestCase):
-    def test_ctest_run_queryreate_db(self):
+    def test_run_query(self):
         result = main.run_query()
         self.assertEqual(result, "Success", "Failed to run the query")
 
